e101612f748aaed2a2af247113ac98859e3caa12
quieoo <quieoo@DESKTOP-H23O3AH.localdomain>
2024-04-24 10:51:06 +0800

    cp pgm index headers

--- src/example.cpp
+++ src/example.cpp
@@ -3,8 +3,32 @@
 #include "include/pthash.hpp"
 #include "src/util.hpp"  // for functions distinct_keys and check
 
+#include "pgm/pgm_index.hpp"
+
+void test_pgm(){
+    // Generate some random data
+    std::vector<int> data(1000000);
+    std::generate(data.begin(), data.end(), std::rand);
+    data.push_back(42);
+    std::sort(data.begin(), data.end());
+
+    // Construct the PGM-index
+    const int epsilon = 128; // space-time trade-off parameter
+    pgm::PGMIndex<int, epsilon> index(data);
+
+    // Query the PGM-index
+    auto q = 42;
+    auto range = index.search(q);
+    auto lo = data.begin() + range.lo;
+    auto hi = data.begin() + range.hi;
+    std::cout << *std::lower_bound(lo, hi, q)<<std::endl;
+    std::cout<<"pgm index test done"<<std::endl;
+}
+
 int main() {
     using namespace pthash;
+
+    test_pgm();
 
     /* Generate 10M random 64-bit keys as input data. */
     static const uint64_t num_keys = 10000000;
